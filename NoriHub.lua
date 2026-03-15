d63e63991c08dda969201f7e1127423b4e58edd6
phantienpye-alt <phantienpye@gmail.com>
2026-03-15 21:24:26 +0700

    repeat wait() until game:IsLoaded() and game.Players.LocalPlayer  print("Nori Hub Loaded")

---
+++ NoriHub.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "HyVpQrSdUvWzYzAbC47fXuIjKl"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/phantienpye-alt/NoriHub/main/NoriHub.lua"))()
